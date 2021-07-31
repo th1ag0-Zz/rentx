4e60a7225bf124c305408af66f1f032a4ad659cf
Thiago <thiagoF89.tf@gmail.com>
2021-07-31 13:12:53 -0300

    componente CarCard

---
+++ src/components/CarCard/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import GasolineSvg from '../../assets/gasoline.svg';
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage,
+} from './styles';
+
+interface DataProps {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: string;
+  };
+  thumbnail: string;
+}
+
+interface Props {
+  data: DataProps;
+}
+
+export const CarCard: React.FC<Props> = ({ data }) => {
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`R$ ${data.rent.price}`}</Price>
+          </Rent>
+
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+      </Details>
+
+      <CarImage
+        resizeMode="contain"
+        source={{
+          uri: data.thumbnail,
+        }}
+      />
+    </Container>
+  );
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,32 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { CarCard } from '../../components/CarCard';
 
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export const Home: React.FC = () => {
+  const data = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: '120',
+    },
+    thumbnail: 'https://cdn.picpng.com/audi/audi-red-a5-28597.png',
+  };
+
+  const data2 = {
+    brand: 'PORSCHE',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: '120',
+    },
+    thumbnail:
+      'https://www.freeiconspng.com/uploads/audi-png-transparent-png-12.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -22,6 +44,9 @@ export const Home: React.FC = () => {
           <TotalCars>{'Total: 12 carros'}</TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarCard data={data} />
+      <CarCard data={data2} />
     </Container>
   );
 };
